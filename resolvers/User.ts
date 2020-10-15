c96a82d70db0b140cc03893fee7c5feb07d00f34
zxc <zxc@zxc.com>
2020-10-15 19:40:00 +0800

    user register

--- resolvers/User.ts
+++ resolvers/User.ts
@@ -1,5 +1,6 @@
 import { User } from "./../models/User";
-import { Resolver, Query, Mutation } from "type-graphql";
+import { Resolver, Query, Mutation, Arg } from "type-graphql";
+import argon2 from "argon2";
 
 @Resolver(User)
 export class UserResolver {
@@ -12,8 +13,32 @@ export class UserResolver {
     };
   }
 
+  @Query(() => [User])
+  async getAllUsers() {
+    return User.find();
+  }
+
   @Mutation(() => Boolean)
-  async register() {
-    return true;
+  async register(
+    @Arg("username") username: string,
+    @Arg("password") password: string,
+    @Arg("email") email: string
+  ) {
+    const hashedPassword = await argon2.hash(password);
+
+    const user = User.create({
+      username,
+      password: hashedPassword,
+      email
+    });
+
+    console.log(user);
+
+    try {
+      await user.save();
+      return true;
+    } catch {
+      return false;
+    }
   }
 }

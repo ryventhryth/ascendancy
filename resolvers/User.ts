312f54116773403beb000afb10e985caf24fdea6
zxc <zxc@zxc.com>
2020-10-15 20:36:10 +0800

    user reg validation

--- resolvers/User.ts
+++ resolvers/User.ts
@@ -1,6 +1,45 @@
 import { User } from "./../models/User";
-import { Resolver, Query, Mutation, Arg } from "type-graphql";
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Arg,
+  InputType,
+  Field,
+  ObjectType
+} from "type-graphql";
 import argon2 from "argon2";
+import { validateUserCredentials } from "../utils";
+
+@InputType()
+export class UserRegisterInput {
+  @Field()
+  username: string;
+
+  @Field()
+  password: string;
+
+  @Field()
+  email: string;
+}
+
+@ObjectType()
+class Error {
+  @Field()
+  fieldname: string;
+
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserRegisterResponse {
+  @Field()
+  status: boolean;
+
+  @Field(() => [Error], { nullable: true })
+  errors?: Error[];
+}
 
 @Resolver(User)
 export class UserResolver {
@@ -18,27 +57,50 @@ export class UserResolver {
     return User.find();
   }
 
-  @Mutation(() => Boolean)
+  @Mutation(() => UserRegisterResponse)
   async register(
-    @Arg("username") username: string,
-    @Arg("password") password: string,
-    @Arg("email") email: string
-  ) {
-    const hashedPassword = await argon2.hash(password);
+    @Arg("credentials") credentials: UserRegisterInput
+  ): Promise<UserRegisterResponse> {
+    const user = User.find({ where: { username: credentials.username } });
 
-    const user = User.create({
-      username,
-      password: hashedPassword,
-      email
+    if (user) {
+      return {
+        status: false,
+        errors: [{ fieldname: "uniq", message: "uniq" }]
+      };
+    }
+
+    const errors = await validateUserCredentials(credentials);
+
+    if (errors.length !== 0) {
+      return {
+        status: false,
+        errors
+      };
+    }
+
+    const hashedPassword = await argon2.hash(credentials.password);
+
+    const newUser = User.create({
+      ...credentials,
+      password: hashedPassword
     });
 
-    console.log(user);
-
     try {
-      await user.save();
-      return true;
+      await newUser.save();
+      return {
+        status: true
+      };
     } catch {
-      return false;
+      return {
+        status: false,
+        errors: [
+          {
+            fieldname: "something went wrong",
+            message: "something went wrong"
+          }
+        ]
+      };
     }
   }
 }
--- utils/index.ts
+++ utils/index.ts
@@ -1,1 +1,27 @@
-export const validateUserCredentials = () => {};
+import * as yup from "yup";
+import { UserRegisterInput } from "../resolvers/User";
+
+let userRegisterSchema = yup.object().shape({
+  username: yup.string().required().min(2).max(50),
+  password: yup.string().required().min(2).max(50),
+  email: yup.string().required().email()
+});
+
+const convertYupErrors = (yupErrors: yup.ValidationError) => {
+  let errors: any = [];
+
+  yupErrors.inner.forEach(e =>
+    errors.push({ fieldname: e.path, message: e.message })
+  );
+
+  return errors;
+};
+
+export const validateUserCredentials = async (user: UserRegisterInput) => {
+  try {
+    await userRegisterSchema.validate(user, { abortEarly: false });
+    return [];
+  } catch (e) {
+    return convertYupErrors(e);
+  }
+};

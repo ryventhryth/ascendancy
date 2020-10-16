3b90b7e6d1de8f54e588d098324eff320978558b
zxc <zxc@zxc.com>
2020-10-16 20:05:51 +0800

    client:init

---
+++ client/webpack.common.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.tsx"),
+  output: {
+    path: path.join(__dirname, "src", "dist"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "awesome-typescript-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Ascendancy",
+      filename: "index.html",
+      template: path.join(__dirname, "src", "dist", "index.tpl.html")
+    })
+  ]
+};
--- models/User.ts
+++ models/User.ts
@@ -1,4 +1,4 @@
-import { Field, ObjectType } from "type-graphql";
+import { Field, Int, ObjectType } from "type-graphql";
 import {
   Entity,
   Column,
@@ -11,7 +11,7 @@ import {
 @Entity()
 @ObjectType()
 export class User extends BaseEntity {
-  @Field()
+  @Field(() => Int)
   @PrimaryGeneratedColumn()
   id: number;
 
